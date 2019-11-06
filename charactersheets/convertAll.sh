ec39af081bb19b21a7b97dada4e103d81476f29f
Renaud G <renaud@rolisteam.org>
2019-11-06 18:23:48 +0100

    convert all sheet in v1.9

--- charactersheets/convertAll.sh
+++ charactersheets/convertAll.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
+set -e
+
 convert=/home/renaud/application/mine/rolisteam-character-sheets/charactersheets/fr/convertSheet.py
 
-files=`find . -name "*.rcs" | awk -F '\\\./' '{print $2}'`
+files=`find . -name "*.rcs" | grep -v fixed | awk -F '\\\./' '{print $2}'`
 for line in `echo $files`; 
 do
     if [ $line != ".rcs" ]
     then
         echo "Processing $line" 
         name=`echo $line | awk -F '.' '{print $1}'`; 
-        rm ${name}_fixed.rcs
-        $convert -i $line -o ${name}_fixed.rcs -c; 
-        mv ${name}_fixed.rcs $line
+        rm -f ${name}_fixed.rcs
+        $convert -i $line -o ${name}_fixed.rcs -c;
+       if [ $? -ne 0 ]
+       then
+           exit 1
+       fi
+       mv ${name}_fixed.rcs $line
     fi
 done
